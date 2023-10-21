cf12b5f0b9457626cda5357b90668607e300ab0b
TiagoRoccatagliata <tiagoroccatagliata@gmail.com>
2023-10-21 16:38:41 -0300

    Navbar

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import './Navbar.css'
+import './navbar.css'
+import logo from '../Assets/logo.png'
+
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
+        <div className='nav-logo'>
+            <img className='imagen' src={logo} alt="" />
+            <p>SOLAR MDP</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+
+        </ul>
     </div>
   )
 }
